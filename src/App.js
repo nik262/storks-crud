7270d833786c3287d0193aafe823a7b3074b3a07
nikilchandrasekar <Nikil.Chandrasekar@indusfusion.com>
2022-08-03 21:12:39 +0530

    comments for huss bhai

--- src/App.js
+++ src/App.js
@@ -25,6 +25,8 @@ import Home from './pages/Home'
 import Jobs from './pages/Jobs'
 import PostaJob from './pages/PostaJob'
 
+//connecting to hasura console using admin password
+//this is very very unsafe need to change this
 const client = new ApolloClient({
   cache : new InMemoryCache(),
   link : new HttpLink({ 
@@ -40,22 +42,40 @@ const client = new ApolloClient({
 
 function App() {
   return (
-    <ApolloProvider client={client}>
 
+    //<apolloprovide> wraps the app so that we can send and recieve state changes from our db(in hasura)
+    <ApolloProvider client={client}>
+    {/*router component is used so that we can have multi page functionality,
+      you wrap the whole app with this router so that the functionality can work
+     */}
     <Router>
+
+     {/*
+      this is the main div of the app can start styling from here
+     */}
     <div className='App'>
       
       <h1 >I will stork</h1>
+
+      {/*
+        navigation component also needs styling, refer to ./components/navigation and style the html there for it to be affected here
+     */}
       <Navigation></Navigation>
 
       <br />
 
       <Switch>
         <Route exact path="/">
+          {/*
+            home page component also needs styling refer to ./pages/home
+          */}
           <Home />
         </Route>
 
         <Route path="/jobs">
+          {/*
+            jobs page needs styling but dont do it rn
+          */}
           <Jobs />
         </Route>
 
--- src/pages/Jobs.jsx
+++ src/pages/Jobs.jsx
@@ -2,7 +2,7 @@ import  React from 'react';
 import {useQuery,  } from '@apollo/client';
 import gql from 'graphql-tag';
 
-
+// this is the GRAPHQL QUERY TO GET THE JOB TITLE AND JOB CATEGORY
 const JOBS = gql`
 query Search {
   Jobs {
@@ -17,11 +17,13 @@ query Search {
 
 function Jobs() {
 
+  //THIS IS WHERE IT QUERIES IT 
   const {loading, error, data } = useQuery(JOBS);
 
   if(loading)  return <p>Loading...</p>
   if(error)  return <p>error try again</p>
 
+  // MAPS QUERY RESULTS AND MAKES THE ui (using html) 
   return data.Jobs.map(({jobid, job_title, job_category}) => (
 
     <div key={jobid}>
