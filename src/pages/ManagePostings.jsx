8972d5aafd0ed886a202580b814d8ea55f5e0f0b
nikilchandrasekar <Nikil.Chandrasekar@indusfusion.com>
2022-08-14 19:09:42 +0400

    posting jobs functionality implemented

---
+++ src/pages/ManagePostings.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+
+function ManagePostings() {
+    return (
+      <div>
+        <h2>Post a job</h2>
+
+      </div>
+    );
+    }
+
+export default ManagePostings
---
+++ src/pages/ViewJob.jsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+
+
+function ViewJob() {
+    return (
+      <div>
+
+        
+
+        
+      </div>
+    );
+    }
+
+export default ViewJob
